46f84fd0e4e5c0fd5894a00ae6e6f68fa37cb847
Sora Nature <105337203+soranature@users.noreply.github.com>
2022-06-13 10:17:05 +0900

    Updates

---
+++ _next/static/2gxZvHrZycO7qzilS9o5f/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/77-7bee91024b2d10b4.js","static/chunks/pages/index-ac31bf2937b37b97.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
